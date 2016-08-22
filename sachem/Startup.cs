25209718852960d7e2411c58bcd872a916d7644e
lainessej <josee.lainesse@cll.qc.ca>
2016-08-22 11:33:42 -0400

    Création du projet et ajout de la couche de données (model)

---
+++ sachem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sachem.Startup))]
+namespace sachem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
