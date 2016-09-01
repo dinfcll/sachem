80dd5e70eefdacb28884af128101fb974b02f817
lainessej <josee.lainesse@cll.qc.ca>
2016-09-01 14:19:55 -0400

    Petit coup de pouce

--- sachem/Controllers/ImporterController.cs
+++ sachem/Controllers/ImporterController.cs
@@ -47,6 +47,10 @@ namespace sachem.Controllers
                         file.SaveAs(path);
 
                     }
+                    else
+                    {
+                        isSavedSuccessfully = false;
+                    }
 
                 }
 
