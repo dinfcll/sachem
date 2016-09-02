1d14b265b9e43a90b5eb42a4dd60c9826c8968ef
Dylan Marcotte <fragzserver@gmail.com>
2016-09-02 08:22:51 -0400

    Ajout des messages en tous genres.

--- sachem/Controllers/AccountController.cs
+++ sachem/Controllers/AccountController.cs
@@ -12,11 +12,13 @@ namespace sachem.Controllers
     [Authorize]
     public class AccountController : Controller
     {
+        private readonly SACHEMEntities db = new SACHEMEntities();
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
 
         public AccountController()
         {
+
         }
 
         public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
@@ -72,7 +74,7 @@ namespace sachem.Controllers
 
             // Ceci ne comptabilise pas les échecs de connexion pour le verrouillage du compte
             // Pour que les échecs de mot de passe déclenchent le verrouillage du compte, utilisez shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordSignInAsync(model.NomUsager, model.MP, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -148,8 +150,8 @@ namespace sachem.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
-                var result = await UserManager.CreateAsync(user, model.Password);
+                var user = new ApplicationUser { UserName = model.NomUsager, Email = model.Email };
+                var result = await UserManager.CreateAsync(user, model.MP);
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
@@ -172,92 +174,69 @@ namespace sachem.Controllers
         //
         // GET: /Account/ConfirmEmail
         [AllowAnonymous]
-        public async Task<ActionResult> ConfirmEmail(string userId, string code)
+        public ActionResult Register()
         {
-            if (userId == null || code == null)
-            {
-                return View("Error");
-            }
-            var result = await UserManager.ConfirmEmailAsync(userId, code);
-            return View(result.Succeeded ? "ConfirmEmail" : "Error");
-        }
+            //Si connecté, on ne peux pas s'inscrire
+            if (PAMIdentite.ObtenirTypeUsager(Session) != TypeUsagers.Aucun)
+                return RedirectToAction("Error", "Home", null);
 
-        //
-        // GET: /Account/ForgotPassword
-        [AllowAnonymous]
-        public ActionResult ForgotPassword()
-        {
+            //Récupère les sexes
+            ViewBag.id_Sexe = new SelectList(db.p_Sexe, "id_Sexe", "Sexe");
+            ViewBag.Autorisation = false;
+
             return View();
         }
 
         //
-        // POST: /Account/ForgotPassword
+        // POST: /Account/Premier Acces
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> ForgotPassword(ForgotPasswordViewModel model)
+        public ActionResult Register(Personne personne, bool Autorisation)
         {
-            if (ModelState.IsValid)
+            //Validation dans le controller
+            if (personne.MP == null)
             {
-                var user = await UserManager.FindByNameAsync(model.Email);
-                if (user == null || !(await UserManager.IsEmailConfirmedAsync(user.Id)))
+                ModelState.AddModelError("MP", Messages.U_001);
+                ModelState.AddModelError("ConfirmPassword", Messages.U_001);
+            }
+
+            if (personne.Matricule7 == null)
+                ModelState.AddModelError("Matricule7", Messages.U_001);
+            else if (personne.Matricule7.Length != 7 || !personne.Matricule.All(char.IsDigit))
+                ModelState.AddModelError("Matricule7", Messages.U_004);
+            else if (db.Personne.Any(x => x.Matricule == personne.Matricule && x.MP != null))
+                ModelState.AddModelError(String.Empty, Messages.I_025);
+            else if (!db.Personne.Any(x => x.Matricule == personne.Matricule))
+                ModelState.AddModelError(String.Empty, Messages.I_027);
+            else
+            //Si tous est ok...
+            {
+                //Sort la personne de la BD pour la compléter
+                Personne EtudiantBD = db.Personne.AsNoTracking().Where(x => x.Matricule == personne.Matricule).FirstOrDefault();
+
+                //Erreur si les infos ne concordent pas
+                if (personne.DateNais != EtudiantBD.DateNais || personne.id_Sexe != EtudiantBD.id_Sexe)
+                    ModelState.AddModelError(String.Empty, Messages.I_027);
+                else
                 {
-                    // Ne révélez pas que l'utilisateur n'existe pas ou qu'il n'est pas confirmé
-                    return View("ForgotPasswordConfirmation");
+                    //Mise à jour des infos
+                    EtudiantBD.Courriel = personne.Courriel;
+                    EtudiantBD.MP = personne.MP;
+                    PAMIdentite.EncrypterMPPersonne(ref EtudiantBD);
+                    EtudiantBD.Autorisation = Autorisation;
+
+                    db.Entry(EtudiantBD).State = EntityState.Modified;
+                    db.SaveChanges();
+
+                    ViewBag.Success = Messages.I_026;
+                    ViewBag.id_Sexe = new SelectList(db.p_Sexe, "id_Sexe", "Sexe");
+                    return View();
                 }
-
-                // Pour plus d'informations sur l'activation de la confirmation du compte et la réinitialisation du mot de passe, consultez http://go.microsoft.com/fwlink/?LinkID=320771
-                // Envoyer un message électronique avec ce lien
-                // string code = await UserManager.GeneratePasswordResetTokenAsync(user.Id);
-                // var callbackUrl = Url.Action("ResetPassword", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);		
-                // await UserManager.SendEmailAsync(user.Id, "Réinitialiser le mot de passe", "Réinitialisez votre mot de passe en cliquant <a href=\"" + callbackUrl + "\">ici</a>");
-                // return RedirectToAction("ForgotPasswordConfirmation", "Account");
             }
 
-            // Si nous sommes arrivés là, un échec s’est produit. Réafficher le formulaire
-            return View(model);
-        }
-
-        //
-        // GET: /Account/ForgotPasswordConfirmation
-        [AllowAnonymous]
-        public ActionResult ForgotPasswordConfirmation()
-        {
-            return View();
-        }
-
-        //
-        // GET: /Account/ResetPassword
-        [AllowAnonymous]
-        public ActionResult ResetPassword(string code)
-        {
-            return code == null ? View("Error") : View();
-        }
-
-        //
-        // POST: /Account/ResetPassword
-        [HttpPost]
-        [AllowAnonymous]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> ResetPassword(ResetPasswordViewModel model)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(model);
-            }
-            var user = await UserManager.FindByNameAsync(model.Email);
-            if (user == null)
-            {
-                // Ne révélez pas que l'utilisateur n'existe pas
-                return RedirectToAction("ResetPasswordConfirmation", "Account");
-            }
-            var result = await UserManager.ResetPasswordAsync(user.Id, model.Code, model.Password);
-            if (result.Succeeded)
-            {
-                return RedirectToAction("ResetPasswordConfirmation", "Account");
-            }
-            AddErrors(result);
-            return View();
+            ViewBag.id_Sexe = new SelectList(db.p_Sexe, "id_Sexe", "Sexe");
+            return View(personne);
         }
 
         //
---
+++ sachem/Models/PersonnesMetaData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sachem.Models
+{
+    public class PersonnesMetaData
+    {
+        //Pour valider la confirmation de mot de passe dans la vue, on crée une nouvelle valeur dans le modèle avec la balise
+        //NotMappedAttribute qui ne sera pas sauvegardée sur la BD mais qui pourra être utilisée pour les validations.
+
+            //Extrait du projet PAM
+        [CompareAttribute("MP", ErrorMessage = Messages.C_001)]
+        [NotMappedAttribute]
+        public string ConfirmPassword { get; set; }
+
+        [NotMappedAttribute]
+        public bool SouvenirConnexion { get; set; }
+
+        [NotMappedAttribute]
+        public string NomUtilisateur { get; set; }
+
+        [NotMappedAttribute]
+        public string AncienMotDePasse { get; set; }
+    }
+}
