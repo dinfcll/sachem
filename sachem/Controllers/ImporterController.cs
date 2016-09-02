4ac4cba73e28d45e72070e5f3b65f9db99f9752c
Jose Ouellet <j@w.com>
2016-09-02 08:08:14 -0400

    Reprendre la dernière version

--- sachem/Controllers/ImporterController.cs
+++ sachem/Controllers/ImporterController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 
 namespace sachem.Controllers
 {
@@ -14,15 +17,65 @@ namespace sachem.Controllers
             return View();
         }
 
-        //public ActionResult SaveUploadedFile()
+        [HttpPost]
+        public ActionResult SaveUploadedFile()
+        {
+            bool isSavedSuccessfully = true;
+            string fName = "";
+            try
+            {
+                foreach (string fileName in Request.Files)
+                {
+                    HttpPostedFileBase file = Request.Files[fileName];
+                    //Save file content goes here
+                    fName = file.FileName;
+                    if (file != null && file.ContentLength > 0)
+                    {
+
+                        var originalDirectory = new DirectoryInfo(string.Format("{0}Images\\WallImages", Server.MapPath(@"\")));
+
+                        string pathString = System.IO.Path.Combine(originalDirectory.ToString(), "imagepath");
+
+                        var fileName1 = Path.GetFileName(file.FileName);
+
+                        bool isExists = System.IO.Directory.Exists(pathString);
+
+                        if (!isExists)
+                            System.IO.Directory.CreateDirectory(pathString);
+
+                        var path = string.Format("{0}\\{1}", pathString, file.FileName);
+                        file.SaveAs(path);
+
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                isSavedSuccessfully = false;
+            }
+
+
+            if (isSavedSuccessfully)
+            {
+                return Json(new { Message = fName });
+            }
+            else
+            {
+                return Json(new { Message = "Error in saving file" });
+            }
+        }
+
+        //public ActionResult SaveUploadedFile(HttpFileCollection httpFileCollection)
         //{
         //    bool isSavedSuccessfully = true;
         //    string fName = "";
         //    try
         //    {
-        //        foreach (string fileName in Request.Files)
+        //        foreach (string fileName in httpFileCollection)
         //        {
-        //            HttpPostedFileBase file = Request.Files[fileName];
+        //            HttpPostedFile file = httpFileCollection.Get(fileName);
         //            //Save file content goes here
         //            fName = file.FileName;
         //            if (file != null && file.ContentLength > 0)
@@ -62,6 +115,7 @@ namespace sachem.Controllers
         //        return Json(new { Message = "Error in saving file" });
         //    }
         //}
+
     }
 
 }
