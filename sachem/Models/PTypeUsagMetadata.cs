59a91fb46ef052c71bb9e46183a44f9ad6c63d2c
1565180 <martineau_alex@hotmail.fr>
2016-09-02 12:30:07 -0400

    Correction de .gitignore

---
+++ sachem/Models/PTypeUsagMetadata.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace sachem.Models
+{
+    //instruction pour indiquer que la classe de CoursMetaData est une classe de métadonnée
+    [MetadataType(typeof(PTypeUsagMetadata))]
+    //on doit redéfinir la classe partielle même si on ajout rien. Placé immédiatement avant la classe de métadonnée associée
+    public partial class p_TypeUsag
+    {
+
+
+    }
+
+    //la classe de métadonnée doit suivre immédiatement la redéfinition de classe partielle
+    //Remarquez que cette classe de métadonnées n’est pas partielle.
+    public class PTypeUsagMetadata
+    {
+
+        [Display(Name = "Type d'usager")]
+        public string TypeUsag;
+    }
+
+}
---
+++ sachem/Models/PersonneMetadata .cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace sachem.Models
+{
+    //instruction pour indiquer que la classe de CoursMetaData est une classe de métadonnée
+    [MetadataType(typeof(PersonneMetadata))]
+    //on doit redéfinir la classe partielle même si on ajout rien. Placé immédiatement avant la classe de métadonnée associée
+    public partial class Personne
+    {
+
+
+    }
+
+    //la classe de métadonnée doit suivre immédiatement la redéfinition de classe partielle
+    //Remarquez que cette classe de métadonnées n’est pas partielle.
+    public class PersonneMetadata
+    {
+
+        [Display(Name = "Nom d'usager")]
+        public string NomUsager;
+
+        [Display(Name = "Prénom")]
+        public string Prenom;
+
+        [Display(Name = "Sexe")]
+        public string id_Sexe;
+
+        [Display(Name = "Date de naissance")]
+        public string DateNais;
+
+        [Display(Name = "Type d'usager")]
+        public string id_TypeUsag;
+
+        [Display(Name = "Mot de passe")]
+        public string MP;
+
+    }
+
+}
