fda21cdbe774de5d83ba0e65345cb4be9857223f
usager <usager@G264-20>
2016-09-02 08:00:51 -0400

    Revert "13:46"
    This reverts commit 5f2a899886a61f7fec0edc40f2e382e76a0649a4.

--- sachem/Controllers/ImporterController.cs
+++ sachem/Controllers/ImporterController.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.IO;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace sachem.Controllers
 {
@@ -17,65 +14,15 @@ namespace sachem.Controllers
             return View();
         }
 
-        [HttpPost]
-        public ActionResult SaveUploadedFile()
-        {
-            bool isSavedSuccessfully = true;
-            string fName = "";
-            try
-            {
-                foreach (string fileName in Request.Files)
-                {
-                    HttpPostedFileBase file = Request.Files[fileName];
-                    //Save file content goes here
-                    fName = file.FileName;
-                    if (file != null && file.ContentLength > 0)
-                    {
-
-                        var originalDirectory = new DirectoryInfo(string.Format("{0}Images\\WallImages", Server.MapPath(@"\")));
-
-                        string pathString = System.IO.Path.Combine(originalDirectory.ToString(), "imagepath");
-
-                        var fileName1 = Path.GetFileName(file.FileName);
-
-                        bool isExists = System.IO.Directory.Exists(pathString);
-
-                        if (!isExists)
-                            System.IO.Directory.CreateDirectory(pathString);
-
-                        var path = string.Format("{0}\\{1}", pathString, file.FileName);
-                        file.SaveAs(path);
-
-                    }
-
-                }
-
-            }
-            catch (Exception ex)
-            {
-                isSavedSuccessfully = false;
-            }
-
-
-            if (isSavedSuccessfully)
-            {
-                return Json(new { Message = fName });
-            }
-            else
-            {
-                return Json(new { Message = "Error in saving file" });
-            }
-        }
-
-        //public ActionResult SaveUploadedFile(HttpFileCollection httpFileCollection)
+        //public ActionResult SaveUploadedFile()
         //{
         //    bool isSavedSuccessfully = true;
         //    string fName = "";
         //    try
         //    {
-        //        foreach (string fileName in httpFileCollection)
+        //        foreach (string fileName in Request.Files)
         //        {
-        //            HttpPostedFile file = httpFileCollection.Get(fileName);
+        //            HttpPostedFileBase file = Request.Files[fileName];
         //            //Save file content goes here
         //            fName = file.FileName;
         //            if (file != null && file.ContentLength > 0)
@@ -115,7 +62,6 @@ namespace sachem.Controllers
         //        return Json(new { Message = "Error in saving file" });
         //    }
         //}
-
     }
 
 }
