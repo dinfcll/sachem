40cb44f67bfff652db71866a9955f702872c0fe4
1337471 <anthobc@hotmail.com>
2016-09-08 11:36:17 -0400

    supprimer étudiant OK

---
+++ sachem/Models/PersonneMetaData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace sachem.Models
+{
+    public class PersonneMetadata {
+        [Display(Name = "Date de naissance")]
+        [RegularExpression(@"^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-2][0-9]:[0-5][0-9]:[0-5][0-9]$|^[0-9]{4}\/[0-9]{2}\/[0-9]{2}$"/*, ErrorMessage = Messages.U_007*/)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = @"{0:yyyy\/MM\/dd}")]
+        public global::System.DateTime DateNais;
+
+    }
+ 
+}
