5888e49d7d14c942c00e1309d601becd876b1d59
1241810 <alexys.leclerc@gmail.com>
2016-09-09 13:06:40 -0400

    Changement d'un nom de classe

--- sachem/Models/ValidationPersonnalisee.cs
+++ sachem/Models/ValidationPersonnalisee.cs
@@ -1,16 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace sachem.Models
 {
     public class VerificationDate : ValidationAttribute
     {
-
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-
         {
             int valeur = (int)value;
             if (valeur >= 1967 && valeur <= DateTime.Now.Year + 1)
