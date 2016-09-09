b6992bd7e78eb8e63118b60f1c0a6458c1e0da10
1241810 <alexys.leclerc@gmail.com>
2016-09-09 14:20:49 -0400

    Formatage de chaine et affichage de données

--- sachem/Controllers/ParametresController.cs
+++ sachem/Controllers/ParametresController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using sachem.Models;
 
 namespace sachem.Controllers
 {
     public class ParametresController : Controller
     {
+
+        private readonly SACHEMEntities db = new SACHEMEntities();
         // GET: Parametres
         public ActionResult Index()
         {
@@ -64,10 +67,19 @@ namespace sachem.Controllers
             }
         }
 
-        public ActionResult EditHoraire(int? id)
+        public ActionResult EditHoraire()
         {
-
-            return View();
+            string[] valeurHeure = new string[5];
+            var horaire = db.p_HoraireInscription.First();
+            var session = db.Session.Single(r => r.id_Sess == horaire.id_Sess);
+            var saison = db.p_Saison.Single(r => r.id_Saison == session.id_Saison);
+            valeurHeure[0] = saison.Saison + " " + session.Annee;
+            valeurHeure[1] = horaire.DateDebut.Date.ToString("yyyy/MM/dd");
+            valeurHeure[2] = horaire.DateFin.Date.ToString("yyyy/MM/dd");
+            valeurHeure[3] = horaire.HeureDebut.ToString(@"hh\:mm");
+            valeurHeure[4] = horaire.HeureFin.ToString(@"hh\:mm");
+            
+            return View(Tuple.Create(horaire,valeurHeure));
         }
         // GET: Parametres/Delete/5
         public ActionResult Delete(int id)
