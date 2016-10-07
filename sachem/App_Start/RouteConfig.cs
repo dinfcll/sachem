f5115d64ee18ee150feaac15eed4982c4c7e4de2
Cristian Zubieta <cristianzuce@hotmail.com>
2016-10-07 14:25:01 -0400

    Modifications des commentaires de branche MAJGroupe

--- sachem/App_Start/RouteConfig.cs
+++ sachem/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace sachem
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Equipe", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
