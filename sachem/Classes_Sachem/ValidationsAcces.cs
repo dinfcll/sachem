8cfaa2e24fd103ea9a0f7cc32d7a45dff5acb010
Dylan Marcotte <fragzserver@gmail.com>
2016-10-20 13:45:56 -0400

    Removal of comments

--- sachem/Classes_Sachem/ValidationsAcces.cs
+++ sachem/Classes_Sachem/ValidationsAcces.cs
@@ -6,11 +6,8 @@ using System.Web;
 using sachem.Models;
 using System.Web.Mvc;
 
-//Classe pour les validations, surtout pour l'uniformisation de la sécurité.
-//On utilise un Action Filter Attribute au lieu de répéter les mêmes fonctions partout. 
 namespace sachem.Classes_Sachem
 {
-    //Validation pour le controlleur paramètres
     public class ValidationAccesParametres : ActionFilterAttribute
     {
         static readonly List<TypeUsagers> rolesAcces = new List<TypeUsagers>() { TypeUsagers.Responsable, TypeUsagers.Super };
