c93ea39d498255a16a35174a1f5f796e03383088
Alexandre Venables <a@v.com>
2016-10-28 08:18:11 -0400

    FormatDate Insc

--- sachem/Models/InscriptionMetadata.cs
+++ sachem/Models/InscriptionMetadata.cs
@@ -19,7 +19,7 @@ namespace sachem.Models
         [Display(Name = "Date d'inscription")]
         //regularExpression pour le format de la date
         [RegularExpression(@"^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-2][0-9]:[0-5][0-9]:[0-5][0-9]$|^[0-9]{4}\/[0-9]{2}\/[0-9]{2}$", ErrorMessage = Messages.U_007)]
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy/MM/dd}")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy\\/MM\\/dd}")]
         public global::System.DateTime DateInscription;
     }
 }
