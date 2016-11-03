51f29813eab55409d08df75c6a20fe7940b3b12e
Dylan Marcotte <fragzserver@gmail.com>
2016-11-03 10:29:05 -0400

    Ajout de la vue Index de Inscription, Controller correspondant, ajout d'une validation dans ValidationsAcces, ajout de la sécurité sur l'index.

---
+++ sachem/Controllers/InscriptionController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using sachem.Classes_Sachem;
+
+namespace sachem.Controllers
+{
+   
+    public class InscriptionController : Controller
+    {
+        [ValidationAcces.ValidationAccesInscription]
+        // GET: Inscription
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+        // GET: Inscription/Delete/5
+        //NOTE: Penser à Wiper les inscriptions à chaque fin de session. Constante?
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Inscription/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
