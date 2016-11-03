1dafa0d5390522827ef2a9083cda3a3fe4ceb1bb
Dylan Marcotte <fragzserver@gmail.com>
2016-11-03 10:51:18 -0400

    Push avant diner

--- sachem/Controllers/InscriptionController.cs
+++ sachem/Controllers/InscriptionController.cs
@@ -4,20 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using sachem.Classes_Sachem;
+using sachem.Models;
 
 namespace sachem.Controllers
 {
    
     public class InscriptionController : Controller
     {
+        private readonly SACHEMEntities db = new SACHEMEntities();
+
         [ValidationAcces.ValidationAccesInscription]
         // GET: Inscription
         public ActionResult Index()
         {
+            ViewBag.id_TypeCourriel = new SelectList(db.p_TypeCourriel, "id_TypeCourriel", "TypeCourriel");
             return View();
         }
 
-        
         // GET: Inscription/Delete/5
         //NOTE: Penser à Wiper les inscriptions à chaque fin de session. Constante?
         public ActionResult Delete(int id)
