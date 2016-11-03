2eac7ce1103f0c2d61b5f5ef6b83e88782f2e7b6
alexandre_venables@hotmail.com <a@v.com>
2016-11-03 13:45:20 -0400

    Modifier transfert pas la donnée

--- sachem/Models/CoursSuiviMetadata.cs
+++ sachem/Models/CoursSuiviMetadata.cs
@@ -34,8 +34,8 @@ namespace sachem.Models
         [Display(Name = "Statut")]
         public string id_Statut;
 
-        [Display(Name = "Résultat")]
-        //[DisplayFormat(DataFormatString = "{0:#\\%}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Résultat (%)")]
+        [DisplayFormat(DataFormatString = @"{0:#\%}", ApplyFormatInEditMode = true)]
         [Range(0, 100 , ErrorMessage = Messages.U_011)]
         public string resultat;
     }
