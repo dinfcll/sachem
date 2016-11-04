511e66d99b07e5ebc8de547cad9cf62293cca45d
Olivier Lafleur <olivier.lafleur@cll.qc.ca>
2016-11-04 08:53:18 -0400

    Ajouter projet de test

---
+++ sachemTests/CoursControllerTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using sachem.Controllers;
+
+namespace sachemTests
+{
+    [TestClass]
+    public class CoursControllerTest
+    {
+        [TestMethod]
+        public void ReferenceTest()
+        {
+            var controller = new CoursController();
+        }
+    }
+}
