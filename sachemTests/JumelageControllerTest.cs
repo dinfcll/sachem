a9e5f8e8fd9e1b15aa804bc75c8bb377366c8f13
Alexandre Venables <alexandre_venables@hotmail.com>
2016-12-01 13:11:14 -0500

    Ajout test

--- sachemTests/JumelageControllerTest.cs
+++ sachemTests/JumelageControllerTest.cs
@@ -11,7 +11,7 @@ namespace sachemTests
     public class JumelageControllerTest
     {
         [TestMethod]
-        public void RetourneNbreJumelageEtudiant()
+        public void TestRetourneNbreJumelageEtudiant()
         {
             var jumelageController = new JumelageController();
             var statut = "";
@@ -26,5 +26,33 @@ namespace sachemTests
             statut = jumelageController.RetourneNbreJumelageEtudiant(3);
             Assert.AreEqual(statut, string.Empty);
         }
+
+        [TestMethod]
+        public void TestRetourneListeJoursSemaine()
+        {
+            var jumelageController = new JumelageController();
+            List<string> joursSem;
+
+            joursSem = jumelageController.RetourneListeJoursSemaine();
+
+            Assert.AreEqual(joursSem[0], "Lundi");
+            Assert.AreEqual(joursSem[1], "Mardi");
+            Assert.AreEqual(joursSem[2], "Mercredi");
+            Assert.AreEqual(joursSem[3], "Jeudi");
+            Assert.AreEqual(joursSem[4], "Vendredi");
+        }
+
+        [TestMethod]
+        public void ShowNullInscription()
+        {
+            var jumelageController = new JumelageController();
+
+            var result = jumelageController.Details(null);
+
+            Assert.AreEqual(typeof(HttpStatusCodeResult), result.GetType());
+            Assert.AreEqual((int)HttpStatusCode.BadRequest, ((HttpStatusCodeResult)result).StatusCode);
+        }
+
+
     }
 }
