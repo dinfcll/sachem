ae646c7d66645c2aca0e843c40156b59f12a5e24
DylanMarcotte <dylan.marcotte.prog@gmail.com>
2016-12-02 08:52:37 -0500

    Fix de la validation d'inscription

--- sachem/Classes_Sachem/ValidationsAcces.cs
+++ sachem/Classes_Sachem/ValidationsAcces.cs
@@ -88,9 +88,7 @@ namespace sachem.Classes_Sachem
             }
             private bool ValidationDate(DateTime DateActuelle)
             {
-                var GroupeDeMath = db.GroupeEtudiant.FirstOrDefault(x => x­.id_Etudiant == id);
-                var GroupeActuel = db.Groupe.FirstOrDefault(x => x.id_Groupe == GroupeDeMath.id_Groupe);
-                var Session = db.Session.FirstOrDefault(x => x.id_Sess == GroupeActuel.id_Sess);
+                var Session = db.Session.GroupBy(s => s.id_Sess).Select(s => s.OrderByDescending(c => c.id_Sess).First()).Select(c => new { c.id_Sess}); ;
                 var HoraireActuel = db.p_HoraireInscription.OrderByDescending(x => x.id_Sess).First();
                 if (!(DateActuelle > HoraireActuel.DateDebut && DateActuelle < HoraireActuel.DateFin))
                     return false;
