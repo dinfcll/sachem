58501bc9dc6800bb1c7a88037ed1fa7352e44d3c
Olivier Lafleur <olivier.lafleur@vooban.com>
2016-12-12 14:31:35 -0500

    On continue

--- sachem/Controllers/ImporterController.cs
+++ sachem/Controllers/ImporterController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Configuration;
 using System.IO;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.WebPages;
 using sachem.Models;
@@ -11,52 +10,54 @@ namespace sachem.Controllers
 {
     public class ImporterController : Controller
     {
-        private string IMPORTEDFILESDIRECTORY = ConfigurationManager.AppSettings["RepertoireATraiter"];
-        private int MAXFILES = 10;//nbre de fichier telechargeable
-        private int MAXFILESIZE =20; //la taille maximale du fichier en mb.
-        private string FILEEXTENSION = ".csv"; //en minuscule seulement
+        private readonly string _importedfilesdirectory = ConfigurationManager.AppSettings["RepertoireATraiter"];
+        private const int Maxfiles = 10;
+        private const int Maxfilesize = 20;
+        private const string Fileextension = ".csv";
 
-        // GET: Importer
         [ValidationAccesSuper]
         public ActionResult Index()
         {
-            ViewBag.MAXFILES = MAXFILES;
-            ViewBag.MAXFILESIZE = MAXFILESIZE;
-            ViewBag.FILEEXTENSION = FILEEXTENSION;
+            ViewBag.MAXFILES = Maxfiles;
+            ViewBag.MAXFILESIZE = Maxfilesize;
+            ViewBag.FILEEXTENSION = Fileextension;
+
             return View();
         }
 
         [HttpPost]
         public ActionResult SaveUploadedFile()
         {
-            string fName = "";
-            string message = "";
+            var fName = "";
+            var message = "";
 
             try
             {
                 foreach (string fileName in Request.Files)
                 {
-                    HttpPostedFileBase file = Request.Files[fileName];
-                    fName = file.FileName;
-                    var originalDirectory = new DirectoryInfo(string.Format("{0}"+IMPORTEDFILESDIRECTORY, Server.MapPath(@"\"+ConfigurationManager.AppSettings["RepertoireFichier"]+@"\")));
-                    string pathString = System.IO.Path.Combine(originalDirectory.ToString());
-                    var fileName1 = Path.GetFileName(file.FileName);
-                    bool isDirExists = System.IO.Directory.Exists(pathString);
-                    bool isFileExist = System.IO.File.Exists(pathString+"\\"+fName);
+                    var file = Request.Files[fileName];
+                    if (file != null)
+                    {
+                        fName = file.FileName;
+                        var originalDirectory = new DirectoryInfo(string.Format("{0}"+_importedfilesdirectory, Server.MapPath(@"\"+ConfigurationManager.AppSettings["RepertoireFichier"]+@"\")));
+                        var pathString = Path.Combine(originalDirectory.ToString());
+                        var isDirExists = Directory.Exists(pathString);
+                        var isFileExist = System.IO.File.Exists(pathString+"\\"+fName);
 
-                    if (isFileExist)
-                    {
-                        message = Messages.FichierAvecLeMemeNomExisteDeja(fName);
-                    }
-                    else
-                    {
-                        if (!isDirExists)
+                        if (isFileExist)
                         {
-                            System.IO.Directory.CreateDirectory(pathString);
+                            message = Messages.FichierAvecLeMemeNomExisteDeja(fName);
                         }
+                        else
+                        {
+                            if (!isDirExists)
+                            {
+                                Directory.CreateDirectory(pathString);
+                            }
 
-                        var path = string.Format("{0}\\{1}", pathString, file.FileName);
-                        file.SaveAs(path);
+                            var path = $"{pathString}\\{file.FileName}";
+                            file.SaveAs(path);
+                        }
                     }
                 }
 
@@ -68,7 +69,7 @@ namespace sachem.Controllers
 
             if (message.IsEmpty())
             {
-                return Json(new object[] { new object() }, JsonRequestBehavior.AllowGet);
+                return Json(new[] { new object() }, JsonRequestBehavior.AllowGet);
             }
             else
             {
