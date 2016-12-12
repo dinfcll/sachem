bcb51ee76bb7cac2e19924cf6afdeb45c10086ca
1241810 <alexys.leclerc@gmail.com>
2016-12-11 23:51:12 -0500

    Fix des tests qui ne passaient pas

--- sachem/Controllers/ContratEngagementController.cs
+++ sachem/Controllers/ContratEngagementController.cs
@@ -32,7 +32,7 @@ namespace sachem.Controllers
             
             if (motDePasse != personneConnectee.MP)
             {
-                ModelState.AddModelError(string.Empty, Messages.MotsDePasseDoiventEtreIdentiques);
+                ModelState.AddModelError(string.Empty, Messages.MotsDePasseDoiventEtreIdentiques());
             }
 
             if (!confirmationSignatureContrat)
